4f0ae7da070808ad8c3bf8fce866dc4021e81bb8
yuliyani6 <yuliyaniaprilyani96@gmail.com>
2025-07-23 22:14:28 +0700

    test

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -4,18 +4,24 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
     public static WebDriver driver;
 
     @Before
-    public void setup() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
     }
 
     @After
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
